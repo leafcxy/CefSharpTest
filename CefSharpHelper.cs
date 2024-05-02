62f31903382327b01838659b7d43a00b3f2ce6a1
leafcxy <cxy1337@gmail.com>
2024-05-02 16:18:29 +0800

    end

--- CefSharpHelper.cs
+++ CefSharpHelper.cs
@@ -14,6 +14,18 @@ namespace CefSharpTest
     {
         public static async Task GenerateAndPrintPdf(string htmlContent)
         {
+            string tempPdfPath = await GeneratePdf(htmlContent);
+            // 这里可以添加代码来处理生成的PDF文件，比如直接发送到打印机打印
+            // 注意：实际打印PDF到物理打印机的步骤通常涉及到操作系统特定的API或外部打印服务
+            // 以下仅为示意，具体打印实现会根据你的应用环境有所不同
+            PrintPdf(tempPdfPath);
+
+            // 打印完成后，可以考虑删除临时文件
+            //File.Delete(tempPdfPath);
+        }
+
+        public static async Task<string> GeneratePdf(string htmlContent)
+        {
             // 确保CefSharp已初始化
             if (!Cef.IsInitialized && !Cef.Initialize(new CefSettings()))
             {
@@ -23,7 +35,6 @@ namespace CefSharpTest
             // 使用CefSharp.OffScreen浏览器
             using (var browser = new ChromiumWebBrowser())
             {
-
                 // 加载HTML内容
                 Thread.Sleep(100);
                 browser.LoadHtml(htmlContent);
@@ -37,18 +48,16 @@ namespace CefSharpTest
                     MarginLeft = 0,
                     MarginRight = 0,
                     MarginTop = 0,
-                    //FooterEnabled = false,
-                    //HeaderEnabled = false,
-                    PageWidth = 100 * 1000,//单位微米，网页上的长度单位建议使用毫米mm
-                    PageHeight = 100 * 1000,
+                    PageWidth = 210 * 1000,//单位微米，网页上的长度单位建议使用毫米mm
+                    PageHeight = 297 * 1000,
                     HeaderFooterEnabled = false,
                     BackgroundsEnabled = true, // 是否打印背景颜色和图片
                 };
 
                 // 使用临时文件路径来保存PDF
                 string tempPdfPath = Path.GetTempFileName() + ".pdf";
-
                 Console.WriteLine(tempPdfPath);
+
                 // 生成PDF文件
                 bool success = await browser.PrintToPdfAsync(tempPdfPath, pdfSettings);
                 if (!success)
@@ -56,19 +65,12 @@ namespace CefSharpTest
                     Console.WriteLine("-" + tempPdfPath + "-");
                     throw new Exception("Failed to generate PDF.");
                 }
-
-                // 这里可以添加代码来处理生成的PDF文件，比如直接发送到打印机打印
-                // 注意：实际打印PDF到物理打印机的步骤通常涉及到操作系统特定的API或外部打印服务
-                // 以下仅为示意，具体打印实现会根据你的应用环境有所不同
-                PrintPdfToFile(tempPdfPath);
-
-                // 打印完成后，可以考虑删除临时文件
-                //File.Delete(tempPdfPath);
+                return tempPdfPath;
             }
         }
 
         // 以下是一个示意性的方法，展示如何使用外部进程或服务来打印PDF文件
-        private static void PrintPdfToFile(string pdfFilePath)
+        public static void PrintPdf(string pdfFilePath)
         {
             // 这里可以调用系统默认的PDF阅读器或打印服务来打印PDF
             // 例如，在Windows上可能使用Process.Start来启动打印进程
